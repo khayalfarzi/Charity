8e895553f890d7ffbd01c74e7f8cede1f2885c65
khayalfarzi <xeyalferziyev9@gmail.com>
2019-12-20 16:31:24 +0300

    Initial commit

---
+++ src/main/java/com/company/CharitySpringApp.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharitySpringApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CharitySpringApp.class, args);
+    }
+}
